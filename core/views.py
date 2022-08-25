a96e09c1d636b79bb1533b82af80bad14b88c00b
Won <onatewill42@gmail.com>
2022-08-24 22:03:38 -0300

    alterações feitas com novos caminhos pedidos pelo instrutor.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse(f'<h1>Hello {nome} de {idade} anos</h1>')
+
+def soma(request, num1, num2):
+    return HttpResponse(f'<h1>A Soma de {num1} e {num2} é: {num1 + num2} </h1>')
+
+def multiplicacao(request, num1, num2):
+    return HttpResponse(f'<h1>A Multiplicação de {num1} e {num2} é: {num1 * num2}</h1>')
+
+def divisao(request, num1, num2):
+    return HttpResponse(f'<h1>A Divisão de {num1} e {num2} é: {num1 / num2}</h1>')
+
+def subtracao(request, num1, num2):
+    subtracao = num1 - num2
+    return HttpResponse(f'<h1>A Subtração de {num1} e {num2} é: {subtracao}</h1>')
